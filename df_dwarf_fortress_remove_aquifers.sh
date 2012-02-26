6e75cbac95fc9d74f0a0bbc5a8b575eec0949413
haesken08 <haesken08@gmail.com>
2012-02-25 21:45:45 -0600

    Adding custom embark profiles from LNP, new scripts to install embark profiles and remove aquifers.

--- df_dwarf_fortress_remove_aquifers.sh
+++ df_dwarf_fortress_remove_aquifers.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-raws_path='dwarffortress/df_linux/raw/objects'
-
-for file in dwarffortress/df_linux/raw/objects/*
-do
-    sed s/\[AQUIFER\]//g $file > $file
-done
+cp -r custom/raw dwarffortress/df_linux/
