c2bfc5da0aa97341a23ed1f8262fa74513e09161
haesken08 <haesken08@gmail.com>
2012-03-07 10:46:22 -0600

    Add more custom configs & quickfort blueprints.

--- df_dwarf_fortress.sh
+++ df_dwarf_fortress.sh
@@ -12,10 +12,10 @@ fi
 cd dwarffortress
 
 # Grab a copy of Dwarf Fortress with the Phoebus tileset and extract it.
-if [ ! -f DF_Phoebus.tar.gz ]; then
-    wget $1 -O DF_Phoebus.tar.gz
-fi
-tar -xf DF_Phoebus.tar.gz
+# if [ ! -f DF_Phoebus.tar.gz ]; then
+#     wget $1 -O DF_Phoebus.tar.gz
+# fi
+# tar -xf DF_Phoebus.tar.gz
 
 # Copy the libgl library to the Dwarf Fortress libs dir.
 libgl_path=$(find /usr/{lib,lib32} -iname libgl.so.1 | egrep -v "64|nvidia" | grep mesa)
