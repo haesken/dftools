eebe9291e1356d4a888f16d40c471455387fd726
haesken08 <haesken08@gmail.com>
2012-03-08 23:42:02 -0600

    Make copy_libgl.sh more robust.

--- scripts/dwarf_fortress/copy_libgl.sh
+++ scripts/dwarf_fortress/copy_libgl.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+DF_WORK_DIR=$1
+
 # Copy the libgl library to the Dwarf Fortress libs dir.
 libgl_path=$(find /usr/{lib,lib32} -iname libgl.so.1 | egrep -v "64|nvidia" | grep mesa)
+df_libs_path="$DF_WORK_DIR/df_linux/libs/"
 
-if [ ! -d df_linux/libs ]; then
-    mkdir df_linux/libs
+if [ ! -d $df_libs_path ]; then
+    mkdir -p $df_libs_path
 fi
 
-df_libs_path='df_linux/libs'
 cp $libgl_path $df_libs_path
-echo "Copied: $libgl_path to $df_libs_path/libgl.so.1"
+echo "Copied: $libgl_path to $df_libs_path"
