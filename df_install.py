3caf0564cd2184edb086aebc7f926039dedffaac
haesken08 <haesken08@gmail.com>
2012-03-14 00:10:52 -0500

    Add option to select dir to install to.

--- df_install.py
+++ df_install.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+""" A set of scripts to download and install Dwarf Fortress
+    and Dwarf Therapist, with a few included utilities.
+"""
+
 import argparse
 import os
 import sys
@@ -11,6 +15,7 @@ import install_dwarf_fortress
 import install_dwarf_therapist
 import lazy_newb_embark
 import phoebus
+from dfa_common import ensure_dir
 
 
 def get_args(): #{{{
@@ -20,70 +25,77 @@ def get_args(): #{{{
             description="Install Dwarf Fortress and utilities.",
             formatter_class=argparse.RawTextHelpFormatter)
 
+    parser.add_argument("-dir", "--directory",
+            type=str,
+            default=os.getcwd(),
+            help="Directory to install Dwarf Fortress in.")
+
     parser.add_argument("-df", "--dwarf_fortress",
             action="store_true",
-            help="Install Dwarf Fortress")
+            help="Install Dwarf Fortress.")
 
     parser.add_argument("-ph", "--phoebus",
             action="store_true",
-            help="Install Phoebus tileset")
+            help="Install Phoebus tileset.")
 
     parser.add_argument("-lze", "--lazy_newb_embark",
             action="store_true",
-            help="Install embark profiles from Lazy Newb Pack")
+            help="Install embark profiles from Lazy Newb Pack.")
 
     parser.add_argument("-daq", "--disable_aquifers",
             action="store_true",
-            help="Disable aquifers")
+            help="Disable aquifers.")
 
     parser.add_argument("-dt", "--dwarf_therapist",
             action="store_true",
-            help="Install Dwarf Therapist")
+            help="Install Dwarf Therapist.")
 
     args = parser.parse_args()
     return args #}}}
 
 
 def main(args): #{{{
-    df_dir_root = os.getcwd()
-    df_dir_df = os.path.join(df_dir_root, 'dwarffortress/')
-    bar = 60 * '='
+    """ Run functions for selected args. """
+    ensure_dir(args.directory)
+    df_dir_df = os.path.join(args.directory, 'dwarffortress/')
+
+    divider = 60 * '='
 
     if args.dwarf_fortress: #{{{
-        print bar
+        print divider
         print 'Installing Dwarf Fortress'
         install_dwarf_fortress.install_dwarf_fortress(df_dir_df)
-        print bar
+        print divider
         print 'Dwarf Fortress installed!'
-        print bar #}}}
+        print divider #}}}
 
     if args.phoebus: #{{{
-        print bar
+        print divider
         print 'Installing Phoebus tileset'
-        print bar
+        print divider
         phoebus.install_phoebus(df_dir_df)
         print 'Phoebus tileset installed!'
-        print bar #}}}
+        print divider #}}}
 
     if args.lazy_newb_embark: #{{{
-        print bar
-        lazy_newb_embark.install_lazy_newb_embarks(df_dir_root)
+        print divider
+        lazy_newb_embark.install_lazy_newb_embarks(args.directory)
         print 'Installed Lazy Newb Pack embark profiles!'
-        print bar #}}}
+        print divider #}}}
 
     if args.disable_aquifers: #{{{
-        print bar
+        print divider
         disable_aquifers.disable_aquifers(df_dir_df)
         print 'Disabled aquifers!'
-        print bar #}}}
+        print divider #}}}
 
     if args.dwarf_therapist: #{{{
-        print bar
+        print divider
         print 'Installing Dwarf Therapist'
         install_dwarf_therapist.install_dwarf_therapist()
-        print bar
+        print divider
         print 'Installed Dwarf Therapist!'
-        print bar #}}}
+        print divider #}}}
     #}}}
 
 
