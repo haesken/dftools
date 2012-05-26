8bde5bca71abd9aededf1c7c85ffecea8d1c6ff9
haesken08 <haesken08@gmail.com>
2012-05-26 00:11:56 -0500

    Remove Dwarf Therapist option and update interface.
    Installing Dwarf Therapist should be handled by a package
    manager or installer, so the feature has been removed for
    now.
    
    The interface has also been updated to allow for the installation
    of more tilesets/embark profiles.

--- df_install.py
+++ df_install.py
@@ -41,7 +41,6 @@ import dfa_common
 import dfa_aquifers
 import dfa_dfhack
 import dfa_df
-import dfa_dt
 import dfa_embarks
 import dfa_tilesets
 
@@ -56,47 +55,47 @@ def get_args(): #{{{
     parser.add_argument("-dir", "--directory",
             type=str,
             default=os.path.join(os.getcwd(), 'dwarffortress'),
-            help="Directory to install Dwarf Fortress in.")
+            help="Directory to install Dwarf Fortress to.")
 
     parser.add_argument("-df", "--dwarf_fortress",
             action="store_true",
             help="Install Dwarf Fortress.")
 
-    parser.add_argument("-ph", "--phoebus",
-            action="store_true",
-            help="Install Phoebus tileset.")
+    parser.add_argument("-t", "--tileset",
+            type=str,
+            default="phoebus",
+            help="Install a tileset.\n" +
+                 "Default: phoebus")
 
-    parser.add_argument("-lze", "--lazy_newb_embark",
-            action="store_true",
-            help="Install embark profiles from Lazy Newb Pack.")
+    parser.add_argument("-e", "--embarks",
+            type=str,
+            default="lnp",
+            help="Install embark profiles.\n" +
+                 "Default: lnp")
 
-    parser.add_argument("-daq", "--disable_aquifers",
-            action="store_true",
-            help="Disable aquifers.")
-
-    parser.add_argument("-dt", "--dwarf_therapist",
-            action="store_true",
-            help="Install Dwarf Therapist.")
+    parser.add_argument("-aq", "--aquifers",
+            type=str,
+            default='disable',
+            help="Enable or disable aquifers.\n" +
+                 "Default: disable")
 
     parser.add_argument("-dfh", "--dfhack",
             action="store_true",
-            help="Install dfhack.")
+            help="Install DFHack.")
 
     parser.add_argument("-q", "--quick",
             action="store_true",
-            help="Equivalent to -df -ph -lze -daq -dfh")
+            help="Quick install of some sensible defaults.\n" +
+                 "Equivalent to '-df -t phoebus -e lnp -aq disable'")
 
     args = parser.parse_args()
     return args #}}}
 
 
 def main(args): #{{{
-    """ Run functions for selected args. """
+    """ Run selected options. """
 
-    path_dfa_root = os.getcwd()
-
-    # Path to custom files (embark profiles, init option restores)
-    path_custom = os.path.join(path_dfa_root, 'custom')
+    # platform = sys.platform
 
     # Wrapper directory where downloaded/extracted archives,
     # and the df_linux directory go.
@@ -106,52 +105,32 @@ def main(args): #{{{
     # Actual Dwarf Fortress install directory.
     path_dflinux = os.path.join(path_dwarffortress, 'df_linux')
 
-
     divider = 60 * '='
 
-    if args.dwarf_fortress or args.quick: #{{{
+    if args.dwarf_fortress or args.quick:
         print divider
-        print 'Installing Dwarf Fortress'
         dfa_df.install_dwarf_fortress(path_dwarffortress)
         print divider
-        print 'Dwarf Fortress installed!'
-        print divider #}}}
 
-    if args.phoebus or args.quick: #{{{
+    if args.tileset or args.quick:
         print divider
-        print 'Installing Phoebus tileset'
+        dfa_tilesets.install_tileset(args.tileset, path_dwarffortress)
         print divider
-        dfa_tilesets.install_phoebus(path_dwarffortress)
-        print 'Phoebus tileset installed!'
-        print divider #}}}
 
-    if args.lazy_newb_embark or args.quick: #{{{
+    if args.embarks or args.quick:
         print divider
-        dfa_embarks.install_lazy_newb_embarks(
-                path_custom, path_dflinux)
-        print 'Installed Lazy Newb Pack embark profiles!'
-        print divider #}}}
+        dfa_embarks.install_embarks(args.embarks, path_dflinux)
+        print divider
 
-    if args.disable_aquifers or args.quick: #{{{
+    if args.aquifers or args.quick:
         print divider
-        dfa_aquifers.disable_aquifers(path_dflinux)
-        print 'Disabled aquifers!'
-        print divider #}}}
+        dfa_aquifers.toggle_aquifers(args.aquifers, path_dflinux)
+        print divider
 
-    if args.dfhack: #{{{
+    if args.dfhack:
         print divider
-        print 'Installing dfhack'
         dfa_dfhack.install_dfhack(path_dwarffortress)
-        print 'dfhack installed!'
-        print divider #}}}
-
-    if args.dwarf_therapist: #{{{
         print divider
-        print 'Installing Dwarf Therapist'
-        dfa_dt.install_dwarf_therapist()
-        print divider
-        print 'Installed Dwarf Therapist!'
-        print divider #}}}
     #}}}
 
 
