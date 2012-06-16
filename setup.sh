c21519058979dd3aebe17ff31481ad0d951934ee
haesken08 <haesken08@gmail.com>
2012-06-16 15:01:11 -0700

    Update python dependencies, fix setup script.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-virutalenv --no-site-packages .virtualenv
+virtualenv --no-site-packages .virtualenv
 source .virtualenv/bin/activate
 
 pip install -r develop/python-depends/requirements.txt
