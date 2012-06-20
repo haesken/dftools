cc4aab04b9f33ae0ddd513937f998da36f87f0f5
haesken08 <haesken08@gmail.com>
2012-06-20 14:01:34 -0500

    Adjust build scripts.

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+rm -r binaries/
 rm -r develop/dist/
+
 python develop/pyinstaller/utils/Build.py develop/df_install.spec
 python develop/pyinstaller/utils/Build.py develop/init_editor.spec
 
 mkdir -p binaries/dwarf_fortress_auto
-
 cp develop/dist/* binaries/dwarf_fortress_auto
 cp -r custom binaries/dwarf_fortress_auto
 
-rm binaries/*.tar.gz
 rm binaries/dwarf_fortress_auto/{df_install.exe,init_editor.exe}
+cd binaries
 tar cvzf dwarf_fortress_auto-$(git log --pretty=format:'%h' -n 1)-linux.tar.gz dwarf_fortress_auto/
