bf65763097149be9867887b93aedf71aa7ea878f
haesken08 <haesken08@gmail.com>
2012-06-20 01:54:21 -0500

    Add windows build script.

---
+++ build_windows.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+rm -r develop/build/dist/
+python develop/pyinstaller/utils/Build.py develop/df_install_windows.spec
+python develop/pyinstaller/utils/Build.py develop/init_editor_windows.spec
+
+mkdir -p binaries/dwarf_fortress_auto
+
+cp develop/dist/* binaries/dwarf_fortress_auto
+cp -r custom binaries/dwarf_fortress_auto
+
+cd binaries
+rm *.tar.gz
+tar cvzf dwarf_fortress_auto-$(git log --pretty=format:'%h' -n 1)-linux.tar.gz dwarf_fortress_auto/
